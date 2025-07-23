610b47aa8ff54498f34e9872767cbdba6a5810b5
Manuj Chadha <manujchadha7777@gmail.com>
2025-07-23 13:11:54 +0530

    ui fixes

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import axios from "axios";
-import InvisibleReCAPTCHA from "./InvisibleReCAPTCHA";
 import { useNavigate } from "react-router-dom";
+import InvisibleReCAPTCHA from "./InvisibleRecaptcha";
 
 const Signup = () => {
   const [form, setForm] = useState({ username: "", email: "", password: "" });
